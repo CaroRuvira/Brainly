004392c01f3bd3441601aaf727f4695fad523da7
Alexis Sanz <105322087+AlexisSanz999@users.noreply.github.com>
2023-03-11 19:40:14 -0300

    hola 2

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function hola(numero) {
-    if (numero % 2 === 0) {
-      const binary = [];
-      while (numero > 0) {
-        const module = numero % 2;
-        binary.unshift(module);
-        numero = Math.floor(numero / 2);
-      }
-      return binary.join("");
-    } else {
-      return numero.toString(16).toUpperCase();
+  if (numero % 2 === 0) {
+    const binary = [];
+    while (numero > 0) {
+      const module = numero % 2;
+      binary.unshift(module);
+      numero = Math.floor(numero / 2);
     }
+    return binary.join("");
+  } else {
+    return numero.toString(16).toUpperCase();
   }
-  
-  function alexis() {
-    return {
-      nombre: "Alexis",
-      edad: 23,
-      nacionalidad: "Argentino",
-    };
-  }
+}
+
+function alexis() {
+  return {
+    nombre: "Alexis",
+    edad: 23,
+    nacionalidad: "Argentino",
+  };
+}

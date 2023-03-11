f334bb067c9fa39fcb2bb9912111a745a3a21093
Agvstin6 <agustin.g.ojeda@gmail.com>
2023-03-11 19:38:58 -0300

    functions-added

---
+++ index.js
@@ -0,0 +1,27 @@
+function laCajaDePandora(numero) {
+    // proximamente escribiremos codigo aqui
+    if (numero % 2 === 0) {
+        const binary = []; 
+        while (numero > 0) {
+            const module = numero % 2; 
+
+            binary.unshift(module);
+
+            numero = Math.floor(numero / 2);
+        }
+
+        return binary.join('');
+    }
+    else{
+        return numero.toString(16).toUpperCase()
+    }
+
+}
+
+const agustin = () => {
+    return {
+        name: 'Agustin',
+        nacionality: 'Argentinian',
+        age: 22
+    }
+}

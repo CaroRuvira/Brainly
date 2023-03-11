c0d7e0b74f5a581764e6afee6f6919a3da795ddd
Alexis Sanz <105322087+AlexisSanz999@users.noreply.github.com>
2023-03-11 19:36:57 -0300

    hola

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function laCajaDePandora(numero){
-    console.log(numero);
+function hola(numero) {
+    if (numero % 2 === 0) {
+      const binary = [];
+      while (numero > 0) {
+        const module = numero % 2;
+        binary.unshift(module);
+        numero = Math.floor(numero / 2);
+      }
+      return binary.join("");
+    } else {
+      return numero.toString(16).toUpperCase();
     }
+  }
+  
+  function alexis() {
+    return {
+      nombre: "Alexis",
+      edad: 23,
+      nacionalidad: "Argentino",
+    };
+  }
